eee8ece7f54d903bcc91feb1e9aaf0a58ba4b1e7
ThuHuong20 <130523086+ThuHuong20@users.noreply.github.com>
2023-10-18 09:23:06 +0700

    Update vouchers.service.ts

--- src/modules/vouchers/vouchers.service.ts
+++ src/modules/vouchers/vouchers.service.ts
@@ -67,32 +67,7 @@ export class VouchersService {
 
     }
 
-  }
-
-  async update(id: number) {
-    try {
-      const oldData = (await this.findOne(Number(id))).data
-      console.log("oldData", oldData);
-      const newData = {
-        ...oldData,
-        IsDelete: true
-      }
-      console.log("newData", newData);
-      const resutl = await this.voucherSer.update(oldData, newData)
-      if (!resutl) return false
-      return {
-        status: true,
-        message: "Delete Successfull ",
-        data: resutl
-      }
-    } catch {
-      return {
-        status: false,
-        message: "Delete Faild ",
-        data: null
-      }
-    }
-    
+  }   
   async update(id: number,updateVoucherDto: UpdateVoucherDto) {
    try{
     const oldData = await this.voucherSer.findOne({
