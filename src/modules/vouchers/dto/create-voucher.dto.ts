b11a5973347dd7d7cac6533748faacabee474782
thanhthu1410 <thanhthu9c@gmail.com>
2023-11-02 09:16:03 +0700

    update swagger voucher

--- src/modules/vouchers/dto/create-voucher.dto.ts
+++ src/modules/vouchers/dto/create-voucher.dto.ts
@@ -1,21 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { Allow, IsNotEmpty } from "class-validator"
 
 export class CreateVoucherDto {
+    @ApiProperty()
     @IsNotEmpty()
     title: string
 
+    @ApiProperty()
     @IsNotEmpty()
     code: string
 
+    @ApiProperty()
     @IsNotEmpty()
     value: number
 
+    @ApiProperty()
     @IsNotEmpty()
     discountType: string
 
-    
+    @ApiProperty()    
     startAt: string
-    
+
+    @ApiProperty()
     endAt: string
     
 
